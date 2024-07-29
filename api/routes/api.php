e1f83936fdd7d88f55e6073ae0e6dd7966b855b5
Arthur <arthurferreira1417@gmail.com>
2024-07-29 19:09:54 -0300

    Faet: criando endpoitns

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController; 
+use App\Http\Controllers\ImageController; 
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/login', [UserController::class, 'LOGIN']);
+Route::delete('/logout', [UserController::class, 'Delete'])->middleware('jwt');
+Route::get('/image/{id}', [ImageController::class, 'getimage'])->middleware('jwt');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::any('{any}', function(){
+    return data([], 400);
+})->middleware('jwt');
