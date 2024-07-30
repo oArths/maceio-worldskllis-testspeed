dd2f28a41449093ea4a24534b2cf62b591e958e0
Arthur <arthurferreira1417@gmail.com>
2024-07-30 15:14:44 -0300

    Faet: criando rotas de listagem individuas de peças além de pesquisa por categoria e atualização de maquina

--- api/routes/api.php
+++ api/routes/api.php
@@ -18,9 +18,19 @@ use App\Http\Controllers\machineController;
 Route::post('/login', [UserController::class, 'LOGIN']);
 Route::delete('/logout', [UserController::class, 'Delete'])->middleware('jwt');
 Route::get('/image/{id}', [ImageController::class, 'getimage']); 
-Route::get('/{op}/{pagesize?}/{page?}', [machineController::class, 'MachinePieces'])->middleware('jwt'); 
-Route::delete('/machines/{id}', [machineController::class, 'deleteMachine']); 
-Route::post('/machines', [machineController::class, 'CreatMachine']); 
+Route::delete('/machines/{id}', [machineController::class, 'deleteMachine'])->middleware('jwt'); 
+Route::put('/machines/{id}', [machineController::class, 'updateMachine'])->middleware('jwt'); 
+Route::post('/machines', [machineController::class, 'CreatMachine'])->middleware('jwt'); 
+Route::get('/search/{category?}/{q?}/{pageSize?}/{page?}', [machineController::class, 'Search'])->middleware('jwt');
+
+Route::get('/motherboards/{pagesize?}/{page?}', [machineController::class, 'MachinePieces'])->middleware('jwt'); 
+Route::get('/processors/{pagesize?}/{page?}', [machineController::class, 'MachinePieces'])->middleware('jwt'); 
+Route::get('/ram-memories/{pagesize?}/{page?}', [machineController::class, 'MachinePieces'])->middleware('jwt'); 
+Route::get('/storage-devices/{pagesize?}/{page?}', [machineController::class, 'MachinePieces'])->middleware('jwt'); 
+Route::get('/graphic-cards/{pagesize?}/{page?}', [machineController::class, 'MachinePieces'])->middleware('jwt'); 
+Route::get('/power-supplies/{pagesize?}/{page?}', [machineController::class, 'MachinePieces'])->middleware('jwt'); 
+Route::get('/machines/{pagesize?}/{page?}', [machineController::class, 'MachinePieces'])->middleware('jwt'); 
+Route::get('/brands/{pagesize?}/{page?}', [machineController::class, 'MachinePieces'])->middleware('jwt'); 
 
 Route::any('{any}', function(){
     return data([], 400);
