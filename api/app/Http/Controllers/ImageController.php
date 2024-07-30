822c53857286d57b85246a3968541956e435b573
Arthur <arthurferreira1417@gmail.com>
2024-07-29 21:48:00 -0300

    Faet: crinado controller pra devoçver imagens

--- api/app/Http/Controllers/ImageController.php
+++ api/app/Http/Controllers/ImageController.php
@@ -10,31 +10,37 @@ use App\Models\processor;
 use App\Models\rammemory;
 use App\Models\storagedevice;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ImageController extends Controller
 {
-    public function getimage($id){
+    public function getimage($id)
+    {
 
         $id = $id ?? null;
-        
-        $image = graphiccard::where('imageUrl', $id)->union(machine::where('imageUrl', $id))
-        ->union(motherboard::where('imageUrl', $id))->union(powersupply::where('imageUrl', $id))
-        ->union(processor::where('imageUrl', $id))->union(rammemory::where('imageUrl', $id))
-        ->union(storagedevice::where('imageUrl', $id))->get();
-        if(!$image){
+        if (!$id) {
             return data(['messsage' => 'Imagem não encontrada'], 404);
-            
+        }
+        $image =
+            DB::table('graphiccard')->select('imageUrl')->where('imageUrl', $id)
+            ->union(DB::table('machine')->select('imageUrl')->where('imageUrl', $id))
+            ->union(DB::table('motherboard')->select('imageUrl')->where('imageUrl', $id))
+            ->union(DB::table('powersupply')->select('imageUrl')->where('imageUrl', $id))
+            ->union(DB::table('rammemory')->select('imageUrl')->where('imageUrl', $id))
+            ->union(DB::table('processor')->select('imageUrl')->where('imageUrl', $id))
+            ->union(DB::table('storagedevice')->select('imageUrl')->where('imageUrl', $id))->first();
+
+        if (empty($image)) {
+            return data(['messsage' => 'Imagem não encontrada'], 404);
         }
         $path = public_path("/images//{$id}") . '.png';
         $file = file_get_contents($path);
 
-        if(!$file){
+        if (!$file) {
             return data(['messsage' => 'Imagem não encontrada'], 404);
         }
         $typ = mime_content_type($path);
-        
-        header("Content-type : $typ");
 
-        return $file;
+        return response($file,200)->header("Content-type", $typ);
     }
 }
