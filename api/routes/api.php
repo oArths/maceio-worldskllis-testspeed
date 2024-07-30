1430c2618059a0a668a17bd446cfeee44f0b3ce1
Arthur <arthurferreira1417@gmail.com>
2024-07-29 21:50:00 -0300

    Faet: crinado endpoits

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController; 
 use App\Http\Controllers\ImageController; 
+use App\Http\Controllers\machineController; 
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,7 +17,10 @@ use App\Http\Controllers\ImageController;
 */
 Route::post('/login', [UserController::class, 'LOGIN']);
 Route::delete('/logout', [UserController::class, 'Delete'])->middleware('jwt');
-Route::get('/image/{id}', [ImageController::class, 'getimage'])->middleware('jwt');
+Route::get('/image/{id}', [ImageController::class, 'getimage']); 
+Route::get('/{op}/{pagesize?}/{page?}', [machineController::class, 'MachinePieces'])->middleware('jwt'); 
+Route::delete('/machines/{id}', [machineController::class, 'deleteMachine']); 
+Route::post('/machines', [machineController::class, 'CreatMachine']); 
 
 Route::any('{any}', function(){
     return data([], 400);
